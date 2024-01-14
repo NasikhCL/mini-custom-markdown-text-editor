0525de83177d53e886c424e867b53c18de34e1b1
Nasikh CL <nasikcl@gmail.com>
2024-01-14 22:57:34 +0530

    new aproch to fix the issue/solve the problem

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -17,58 +17,35 @@ import "draft-js/dist/Draft.css";
 const { hasCommandModifier } = KeyBindingUtil;
 
 const CustomEditor = () => {
+
   const [editorState, setEditorState] = React.useState(() =>
     EditorState.createEmpty()
   );
-const modifyBlockWithType = (newContentState) => {
-    const currentBlocks = newContentState.getBlockMap().toArray();
-    const blockIndexToModify = currentBlocks.findIndex((block) => block.getText().startsWith('# '));
-
-    if (blockIndexToModify !== -1) {
-      const blockToModify = currentBlocks[blockIndexToModify];
-
-      // Use ContentBlock's merge method to create a modified block
-      const modifiedBlock = blockToModify.merge({
-        type: 'header-one', // Set the new type to header-one
-      });
-
-      // console.log(modifiedBlock,'BLOCK.')
-      const newBlocks = currentBlocks.slice(); // Create a shallow copy
-      newBlocks[blockIndexToModify] = modifiedBlock;
-      
-      // console.log(newBlocks,'new bll')
-      const newContentState = ContentState.createFromBlockArray(
-        newBlocks
-      );
-    
-      console.log(newContentState,'newCOntent')
-      // setEditorState(EditorState.push(editorState, newContentState, 'change-block'),);
-     return newContentState;
-    }
-    return null;
 
 
-  };
 
   const handleEditorState = (editorState) => {
-    const newContentState = editorState.getCurrentContent();
-    const newBlock = modifyBlockWithType(newContentState);
-    console.log(newBlock,'content block')
-    if(newBlock){
-      const newEditorState = EditorState.push(editorState, newBlock, 'change-block-type');
-
-      console.log('newEditorState:', newEditorState.toJS());
-  
-      setEditorState(newEditorState);
-      // setEditorState(EditorState.push(editorState, newContentState, 'change-block-type'));
-    }else{
-      setEditorState(editorState);
-    }
-    const contentState = editorState.getCurrentContent();
-    const rawContent = convertToRaw(contentState).blocks;
-
-    console.log(rawContent,'raw')
-
+    const currentContent = editorState.getCurrentContent();
+    const blocks = convertToRaw(currentContent).blocks;
+    const newBlocks = []
+    blocks.forEach(block => {
+      // console.log(element.text,'text')
+      if(block.text.startsWith('# ')){
+        console.log('starts with hash and space')
+        block.text=''
+        block.type = 'header-one';
+      }
+      newBlocks.push(block)
+    });
+    const newRawContent = { ...convertToRaw(currentContent), blocks: newBlocks };
+    const newContentState = convertFromRaw(newRawContent);
+    const newEditorState = EditorState.push(editorState, newContentState, 'change-block-type');
+    // this.editor.update(newEditorState);
+    setEditorState(newEditorState);
+    // console.log(newEditorState,)
+    // setEditorState(editorState);
+    // console.log(blocks) 
+    
   };
 
   const handleButtonClick = () => {
@@ -100,21 +77,24 @@ const modifyBlockWithType = (newContentState) => {
     // setEditorState(RichUtils.toggleBlockType(editorState, 'header-one'))
   };
 
-  useEffect(() => {
-    // Function to load editor content
-    const rawContent = localStorage.getItem("draftContent");
+  // useEffect(() => {
+  //   // Function to load editor content
+  //   const rawContent = localStorage.getItem("draftContent");
 
-    if (rawContent) {
-      const contentState = JSON.parse(rawContent);
-      const editorState = EditorState.createWithContent(contentState);
-      setEditorState(editorState);
-    }
-  }, []);
+  //   if (rawContent) {
+  //     const contentState = JSON.parse(rawContent);
+  //     const editorState = EditorState.createWithContent(contentState);
+  //     setEditorState(editorState);
+  //   }
+  // }, []);
 
   return (
     <div className="border w-full p-4">
       <button onClick={handleButtonClick}>CHECK</button>
-      <Editor editorState={editorState} onChange={handleEditorState} />
+      <Editor
+        editorState={editorState}
+        onChange={handleEditorState}
+      />
     </div>
   );
 };
