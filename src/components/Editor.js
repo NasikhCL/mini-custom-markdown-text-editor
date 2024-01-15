f85e4759a1e9e45dfcdb5d53df0ff3ebcdc59d26
Nasikh CL <nasikcl@gmail.com>
2024-01-15 12:27:10 +0530

    good checkpoint: header-one major issue solved(minor issues exists)

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Editor,
   EditorState,
@@ -23,37 +23,95 @@ const CustomEditor = () => {
   );
 
 
+  const [headerOneArr, setHeaderOneArr] =useState([])
+// console.log('com re render')
+  const handleEditorState = (editorState,char) => {
 
-  const handleEditorState = (editorState) => {
     const currentContent = editorState.getCurrentContent();
     const blocks = convertToRaw(currentContent).blocks;
+    // console.log(char,'char')
     const newBlocks = []
     blocks.forEach(block => {
+      // console.log(headerOneArr,'array')
       // console.log(element.text,'text')
-      if(block.type === 'header-one'){
-        newBlocks.push(block)
-      }else if(block.text.startsWith('# ')){
-        console.log('starts with hash and space')
-        block.text=''
-        block.type = 'header-one';
-        newBlocks.push(block)
-      }else{
-        block.type = 'unstyled'  
+      const isHeaderOne = headerOneArr.includes(block.key)
+      console.log(isHeaderOne)
+      if(!isHeaderOne && block.text === ''){
+        block.type ='unstyled'
+        newBlocks.push(block);
+      }else if(isHeaderOne){
+        block.style = 'header-one'
         newBlocks.push(block)
       }
+      else if(block.text.startsWith('# ')){
+        setHeaderOneArr(prev=> [...prev, block.key])
+        block.type = 'header-one'
+        block.text = ''
+        newBlocks.push(block);
+        console.log('pushed', headerOneArr)
+      }else{
+          block.type = 'unstyled'  
+          newBlocks.push(block)
+        }
+      
+
+
+      // if(!isHeaderOne && block.text === ''){
+      //     block.type = 'unstyled'
+      //     newBlocks.push(block)
+      //     console.log('reached here//////////')
+      // }
+      // else if(block.text === ' '){
+      //   block.type = 'unstyled'
+      //   newBlocks.push(block);
+      //   if(isHeaderOne){
+      //     headerOneArr.splice(headerOneArr.indexOf(block.key),1)
+      //   }
+      // }
+      // else if(block.type === 'header-one'){
+      //   newBlocks.push(block)
+      //   if(!isHeaderOne){
+      //     headerOneArr.push(block.key);
+      //   }
+      // }
+      // else if(block.text.startsWith('# ')){
+      //   if(!isHeaderOne){
+      //     headerOneArr.push(block.key);
+      //     console.log('pushed',headerOneArr)
+
+      //   }
+      //   console.log('starts with hash and space')
+      //   block.text=''
+      //   block.type = 'header-one';
+      //   newBlocks.push(block) 
+      // }else{
+      //   block.type = 'unstyled'  
+      //   newBlocks.push(block)
+      // }
     });
     const newRawContent = { ...convertToRaw(currentContent), blocks: newBlocks };
     const newContentState = convertFromRaw(newRawContent);
     const newEditorState = EditorState.push(editorState, newContentState, 'change-block');
-    // this.editor.update(newEditorState);
     const selectionState = editorState.getSelection();
     const newEditorStateWithHandleCursor = EditorState.forceSelection(newEditorState, selectionState);
     setEditorState(newEditorStateWithHandleCursor);
+    
     // console.log(newEditorState,)
     // setEditorState(editorState);
     console.log(blocks) 
     
   };
+  
+
+ 
+
+    
+
+  const onChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+
+
   
 
   const handleButtonClick = () => {
@@ -102,8 +160,10 @@ const CustomEditor = () => {
       <Editor
         editorState={editorState}
         onChange={handleEditorState}
-        preserveSelectionOnBlur={true} 
-
+        // handleBeforeInput={handleKeyDown}
+        // preserveSelectionOnBlur={true} 
+        // keyBindingFn={myKeyBindingFn}
+        // handleReturn={handleReturn}
       />
     </div>
   );
